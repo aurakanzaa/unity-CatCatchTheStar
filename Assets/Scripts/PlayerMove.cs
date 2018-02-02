e4bbdefd743c82f47ceab54d1742656f9ae5faf0
aurakanzaa <aurakanzaaa@gmail.com>
2018-02-02 10:29:38 +0700

    done

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+
+    public float speed = 10;
+    private Rigidbody2D rigidBody2D;
+    private Animator myAnimator;
+    [SerializeField]
+    private Transform[] groundPoints;
+    [SerializeField]
+    private float groundRadius;
+
+    [SerializeField]
+    private LayerMask whatIsGround;
+
+    private bool isGrounded;
+
+    private bool jump;
+
+    [SerializeField]
+    private float jumpForce;
+
+    void Awake()
+    {
+        rigidBody2D = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+
+    }
+    void FixedUpdate()
+    {
+        isGrounded = IsGrounded();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+        float xMove = Input.GetAxis("Horizontal");
+        Movement(xMove);
+
+        resetValues();
+    }
+    private void Movement(float xMove)
+    {
+        if (isGrounded && jump)
+        {
+            isGrounded = false;
+            rigidBody2D.AddForce(new Vector2(0, jumpForce));
+        }
+        rigidBody2D.velocity = new Vector2(xMove * speed, rigidBody2D.velocity.y);
+        myAnimator.SetFloat("speed", Mathf.Abs(xMove));
+    }
+    private void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+        }
+    }
+    private void resetValues()
+    {
+        jump = false;
+    }
+    private bool IsGrounded()
+    {
+        if (rigidBody2D.velocity.y <= 0)
+        {
+            foreach (Transform point in groundPoints)
+            {
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(point.position, groundRadius, whatIsGround);
+
+                for (int i = 0; i < colliders.Length; i++)
+                {
+                    if (colliders[i].gameObject != gameObject)
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+}
---
+++ Assets/Scripts/SliderValueToText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueToText : MonoBehaviour {
+
+    public Slider sliderUI;
+    private Text textSliderValue;
+
+	// Use this for initialization
+	void Start () {
+        textSliderValue = GetComponent<Text>();
+        ShowSliderValue();
+	}
+	
+	// Update is called once per frame
+	public void ShowSliderValue () {
+        string sliderMessage = "Slider value =" + sliderUI.value;
+        textSliderValue.text = sliderMessage;
+	}
+}
